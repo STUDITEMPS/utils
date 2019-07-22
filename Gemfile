96902097e1fb277dc68bb34195f858c1fe83db9b
Sascha Hoellger <mitnal@castle-grayskull.org>
2019-07-22 14:53:14 +0200

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in studitemps-utils.gemspec
+gemspec
+
+gem 'rubocop'
---
+++ lib/studitemps/utils.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'studitemps/utils/version'
+
+module Studitemps
+  module Utils
+    class Error < StandardError; end
+  end
+end
