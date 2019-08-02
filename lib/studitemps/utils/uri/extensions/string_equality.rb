bcfcdd961e25bd02191048e86ed415a38cc88d12
Sascha Hoellger <mitnal@castle-grayskull.org>
2019-08-02 17:23:05 +0200

    Adds string equality extension documentation.

--- lib/studitemps/utils/uri/extensions/string_equality.rb
+++ lib/studitemps/utils/uri/extensions/string_equality.rb
@@ -6,13 +6,29 @@ module Studitemps
   module Utils
     module URI
       module Extensions
+        # Allows URIs to be compared to strings.
+        #
+        # @example
+        #   MyURI = Studitemps::Utils::URI.build(schema: 'com.example')
+        #   uri = MyURI.new(context: 'billing', resource: 'invoice', id: 42)
+        #   uri == 'com.example:billing:invoice:42' # => true
         module StringEquality
 
+          # Included instance methods.
           module InstanceMethods
+
+            # Compares other with own string representation if other is a string.
+            #
+            # @param other [String] string to compare own string representation with.
+            # @return [Boolean] if own string representation equals other
             def eql?(other)
               other.is_a?(String) ? to_s.eql?(other) : super
             end
 
+            # Compares other with own string representation if other is a string.
+            #
+            # @param other [String] string to compare own string representation with.
+            # @return [Boolean] if own string representation equals other
             def ==(other)
               other.is_a?(String) ? to_s == other : super
             end
