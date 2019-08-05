406060c95c78ceda2169649183ca1daebabba537
Sascha Hoellger <mitnal@castle-grayskull.org>
2019-08-05 11:26:39 +0200

    Adds base uri documentation.

--- lib/studitemps/utils/uri/base.rb
+++ lib/studitemps/utils/uri/base.rb
@@ -3,6 +3,8 @@
 module Studitemps
   module Utils
     module URI
+      # URI base class.
+      # @abstract
       class Base
         class InvalidURI < StandardError; end
         extend Dry::Core::ClassAttributes
@@ -13,16 +15,50 @@ module Studitemps
         defines :context
         defines :resource
 
+        # Returns string representation of URI.
+        #
+        # @example
+        #   ExampleURI = Studitemps::Utils::URI.build(schema: 'com.example')
+        #   uri = ExampleURI.new(context: 'billing', resource: 'invoice', id: 'R422342')
+        #   uri.to_s # => 'com.example:billing:invoice:R422342'
+        #
+        # @return [String] string representation of URI
         def to_s
           [schema, context, resource, id].compact.join(':')
         end
         alias to_str to_s
 
+        # Nicer inspect for URI.
+        #
+        # @return [String] inspect of URI
         def inspect
           "#<#{self.class.name} '#{self}'>"
         end
 
         class << self
+          # Build a new URI from the given value.
+          #
+          # @example
+          #   ExampleURI = Studitemps::Utils::URI.build(schema: 'com.example')
+          #   uri = ExampleURI.new(context: 'billing', resource: 'invoice', id: 'R422342')
+          #
+          #   ExampleURI.build(uri)
+          #   # => #<ExampleURI 'com.example:billing:invoice:R422342'>
+          #   ExampleURI.build(context: 'billing', resource: 'invoice', id: 'R422342')
+          #   # => #<ExampleURI 'com.example:billing:invoice:R422342'>
+          #   ExampleURI.build('com.example:billing:invoice:R422342')
+          #   # => #<ExampleURI 'com.example:billing:invoice:R422342'>
+          #
+          #   # Appropriate URIs can be cast
+          #   BillingURI =  Studitemps::Utils::URI.build(from: ExampleURI, context: 'billing')
+          #   BillingURI.build(uri) # => #<BillingURI 'com.example:billing:invoice:R422342'>
+          #
+          #   PayrollURI = Studitemps::Utils::URI.build(from: ExampleURI, context: 'payroll')
+          #   PayrollURI.build(uri) # => raises Studitemps::Utils::URI::Base::InvalidURI
+          #
+          # @raise [InvalidURI] if value can not be converted to URI
+          # @param [Base, Hash, String] value to build URI from
+          # @return [Base] new URI from given value
           def build(value)
             case value
             when ::Studitemps::Utils::URI::Base then from_uri(value)
